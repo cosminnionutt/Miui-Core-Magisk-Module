d13edb014511a08a6b324fd8f7b71cc19ac40fa9
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-03-19 15:25:15 +0200

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,402 @@
+# space
+ui_print " "
+
+# var
+UID=`id -u`
+LIST32BIT=`grep_get_prop ro.product.cpu.abilist32`
+if [ ! "$LIST32BIT" ]; then
+  LIST32BIT=`grep_get_prop ro.system.product.cpu.abilist32`
+fi
+
+# log
+if [ "$BOOTMODE" != true ]; then
+  FILE=/data/media/"$UID"/$MODID\_recovery.log
+  ui_print "- Log will be saved at $FILE"
+  exec 2>$FILE
+  ui_print " "
+fi
+
+# optionals
+OPTIONALS=/data/media/"$UID"/optionals.prop
+if [ ! -f $OPTIONALS ]; then
+  touch $OPTIONALS
+fi
+
+# debug
+if [ "`grep_prop debug.log $OPTIONALS`" == 1 ]; then
+  ui_print "- The install log will contain detailed information"
+  set -x
+  ui_print " "
+fi
+
+# run
+. $MODPATH/function.sh
+
+# info
+MODVER=`grep_prop version $MODPATH/module.prop`
+MODVERCODE=`grep_prop versionCode $MODPATH/module.prop`
+ui_print " ID=$MODID"
+ui_print " Version=$MODVER"
+ui_print " VersionCode=$MODVERCODE"
+if [ "$KSU" == true ]; then
+  ui_print " KSUVersion=$KSU_VER"
+  ui_print " KSUVersionCode=$KSU_VER_CODE"
+  ui_print " KSUKernelVersionCode=$KSU_KERNEL_VER_CODE"
+  sed -i 's|#k||g' $MODPATH/post-fs-data.sh
+else
+  ui_print " MagiskVersion=$MAGISK_VER"
+  ui_print " MagiskVersionCode=$MAGISK_VER_CODE"
+fi
+ui_print " "
+
+# bit
+if [ "$IS64BIT" == true ]; then
+  ui_print "- 64 bit architecture"
+  ui_print " "
+  # 32 bit
+  if [ "$LIST32BIT" ]; then
+    ui_print "- 32 bit library support"
+  else
+    ui_print "- Doesn't support 32 bit library"
+    rm -rf $MODPATH/armeabi-v7a $MODPATH/x86\
+     $MODPATH/system*/lib $MODPATH/system*/vendor/lib
+  fi
+  ui_print " "
+else
+  ui_print "- 32 bit architecture"
+  rm -rf `find $MODPATH -type d -name *64*`\
+   $MODPATH/system*/bin
+  ui_print " "
+fi
+
+# sdk
+NUM=21
+if [ "$API" -lt $NUM ]; then
+  ui_print "! Unsupported SDK $API."
+  ui_print "  You have to upgrade your Android version"
+  ui_print "  at least SDK $NUM to use this module."
+  abort
+else
+  ui_print "- SDK $API"
+  ui_print " "
+fi
+
+# recovery
+mount_partitions_in_recovery
+
+# magisk
+magisk_setup
+
+# path
+SYSTEM=`realpath $MIRROR/system`
+VENDOR=`realpath $MIRROR/vendor`
+PRODUCT=`realpath $MIRROR/product`
+SYSTEM_EXT=`realpath $MIRROR/system_ext`
+ODM=`realpath $MIRROR/odm`
+MY_PRODUCT=`realpath $MIRROR/my_product`
+
+# sepolicy
+FILE=$MODPATH/sepolicy.rule
+DES=$MODPATH/sepolicy.pfsd
+if [ "`grep_prop sepolicy.sh $OPTIONALS`" == 1 ]\
+&& [ -f $FILE ]; then
+  mv -f $FILE $DES
+fi
+
+# function
+file_check_system() {
+for FILE in $FILES; do
+  DES=$SYSTEM$FILE
+  DES2=$SYSTEM_EXT$FILE
+  if [ -f $DES ] || [ -f $DES2 ]; then
+    ui_print "- Detected $FILE"
+    ui_print " "
+    rm -f $MODPATH/system$FILE
+  fi
+done
+}
+file_check_vendor() {
+for FILE in $FILES; do
+  DES=$VENDOR$FILE
+  DES2=$ODM$FILE
+  if [ -f $DES ] || [ -f $DES2 ]; then
+    ui_print "- Detected $FILE"
+    ui_print " "
+    rm -f $MODPATH/system/vendor$FILE
+  fi
+done
+}
+
+# check
+FILES=/bin/shelld
+file_check_system
+if [ "$IS64BIT" == true ]; then
+  LISTS=`ls $MODPATH/system/lib64`
+  FILES=`for LIST in $LISTS; do echo /lib64/$LIST; done`
+  file_check_system
+  LISTS=`ls $MODPATH/system/vendor/lib64`
+  FILES=`for LIST in $LISTS; do echo /lib64/$LIST; done`
+  file_check_vendor
+fi
+if [ "$LIST32BIT" ]; then
+  LISTS=`ls $MODPATH/system/lib`
+  FILES=`for LIST in $LISTS; do echo /lib/$LIST; done`
+  file_check_system
+  LISTS=`ls $MODPATH/system/vendor/lib`
+  FILES=`for LIST in $LISTS; do echo /lib/$LIST; done`
+  file_check_vendor
+fi
+
+# function
+check_function() {
+ui_print "- Checking"
+ui_print "$NAME"
+ui_print "  function at"
+ui_print "$FILE"
+ui_print "  Please wait..."
+if ! grep -q $NAME $FILE; then
+  ui_print "  Function not found"
+  SYSTEM_10=true
+fi
+ui_print " "
+}
+
+# check
+SYSTEM_10=false
+NAME=_ZN7android23sp_report_stack_pointerEv
+if [ "$IS64BIT" == true ]; then
+  DES=$MODPATH/system/bin/shelld
+  if [ -f $DES ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libshellservice.so||g' -e 's|libshell_jni.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib64/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/lib64/libexmedia.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libexmedia.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib64/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/lib64/libmiuiblur.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libmiuiblur.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib64/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/lib64/libshell.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libshell.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib64/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/vendor/lib64/libcdsprpc.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libcdsprpc.so||g' -e 's|lib%s_skel.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib64/$LIST; done`
+    check_function
+  fi
+fi
+if [ "$LIST32BIT" ]; then
+  DES=$MODPATH/system/lib/libexmedia.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libexmedia.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/lib/libmiuiblur.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+           | sed -e 's|libmiuiblur.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/lib/libshell.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+           | sed -e 's|libshell.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib/$LIST; done`
+    check_function
+  fi
+  DES=$MODPATH/system/vendor/lib/libcdsprpc.so
+  if [ -f $DES ] && [ $SYSTEM_10 != true ]; then
+    LISTS=`strings $DES | grep ^lib | grep .so\
+            | sed -e 's|libcdsprpc.so||g' -e 's|lib%s_skel.so||g'`
+    FILE=`for LIST in $LISTS; do echo $SYSTEM/lib/$LIST; done`
+    check_function
+  fi
+fi
+if [ $SYSTEM_10 == true ]; then
+  ui_print "- Using legacy libraries"
+  cp -rf $MODPATH/system_10/* $MODPATH/system
+  ui_print " "
+fi
+rm -rf $MODPATH/system_10
+
+# extract
+APP=miuisystem
+ui_print "- Extracting..."
+FILE=`find $MODPATH/system -type f -name $APP.apk`
+DIR=$MODPATH/system/etc
+DES=assets/*
+unzip -d $TMPDIR -o $FILE $DES
+cp -rf $TMPDIR/$DES $DIR
+ui_print " "
+
+# cleaning
+ui_print "- Cleaning..."
+PKGS=`cat $MODPATH/package.txt`
+if [ "$BOOTMODE" == true ]; then
+  for PKG in $PKGS; do
+    FILE=`find /data/app -name *$PKG*`
+    if [ "$FILE" ]; then
+      RES=`pm uninstall $PKG 2>/dev/null`
+    fi
+  done
+fi
+rm -rf $MODPATH/unused
+remove_sepolicy_rule
+ui_print " "
+# power save
+FILE=$MODPATH/system/etc/sysconfig/*
+if [ "`grep_prop power.save $OPTIONALS`" == 1 ]; then
+  ui_print "- $MODNAME will not be allowed in power save."
+  ui_print "  It may save your battery but decreasing $MODNAME performance."
+  for PKG in $PKGS; do
+    sed -i "s|<allow-in-power-save package=\"$PKG\"/>||g" $FILE
+    sed -i "s|<allow-in-power-save package=\"$PKG\" />||g" $FILE
+  done
+  ui_print " "
+fi
+
+# function
+conflict() {
+for NAME in $NAMES; do
+  DIR=/data/adb/modules_update/$NAME
+  if [ -f $DIR/uninstall.sh ]; then
+    sh $DIR/uninstall.sh
+  fi
+  rm -rf $DIR
+  DIR=/data/adb/modules/$NAME
+  rm -f $DIR/update
+  touch $DIR/remove
+  FILE=/data/adb/modules/$NAME/uninstall.sh
+  if [ -f $FILE ]; then
+    sh $FILE
+    rm -f $FILE
+  fi
+  rm -rf /metadata/magisk/$NAME
+  rm -rf /mnt/vendor/persist/magisk/$NAME
+  rm -rf /persist/magisk/$NAME
+  rm -rf /data/unencrypted/magisk/$NAME
+  rm -rf /cache/magisk/$NAME
+  rm -rf /cust/magisk/$NAME
+done
+}
+
+# conflict
+NAMES=MIUICore
+conflict
+
+# function
+permissive_2() {
+sed -i 's|#2||g' $MODPATH/post-fs-data.sh
+}
+permissive() {
+FILE=/sys/fs/selinux/enforce
+SELINUX=`cat $FILE`
+if [ "$SELINUX" == 1 ]; then
+  if ! setenforce 0; then
+    echo 0 > $FILE
+  fi
+  SELINUX=`cat $FILE`
+  if [ "$SELINUX" == 1 ]; then
+    ui_print "  Your device can't be turned to Permissive state."
+    ui_print "  Using Magisk Permissive mode instead."
+    permissive_2
+  else
+    if ! setenforce 1; then
+      echo 1 > $FILE
+    fi
+    sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+  fi
+else
+  sed -i 's|#1||g' $MODPATH/post-fs-data.sh
+fi
+}
+
+# permissive
+if [ "`grep_prop permissive.mode $OPTIONALS`" == 1 ]; then
+  ui_print "- Using device Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive
+  ui_print " "
+elif [ "`grep_prop permissive.mode $OPTIONALS`" == 2 ]; then
+  ui_print "- Using Magisk Permissive mode."
+  rm -f $MODPATH/sepolicy.rule
+  permissive_2
+  ui_print " "
+fi
+
+# public
+FILE=$MODPATH/post-fs-data.sh
+if [ "`grep_prop miui.public $OPTIONALS`" != 0 ]; then
+  sed -i 's|#p||g' $FILE
+else
+  ui_print "- Does not patch public.libraries.txt"
+  ui_print "  You will not be able to normal install Miui apps"
+  ui_print " "
+fi
+
+# global
+FILE=$MODPATH/service.sh
+NAME=ro.product.mod_device
+if [ "`grep_prop miui.global $OPTIONALS`" == 1 ]; then
+  ui_print "- Global mode"
+  sed -i "s|#resetprop -n $NAME|resetprop -n $NAME|g" $FILE
+  ui_print " "
+fi
+
+# code
+FILE=$MODPATH/service.sh
+NAME=ro.miui.ui.version.code
+if [ "`grep_prop miui.code $OPTIONALS`" == 0 ]; then
+  ui_print "- Removing $NAME..."
+  sed -i "s|resetprop -n $NAME|#resetprop -n $NAME|g" $FILE
+  ui_print " "
+fi
+
+# media
+if [ ! -d /product/media ] && [ -d /system/media ]; then
+  ui_print "- Using /system/media instead of /product/media"
+  mv -f $MODPATH/system/product/media $MODPATH/system
+  rm -rf $MODPATH/system/product
+  ui_print " "
+elif [ ! -d /product/media ] && [ ! -d /system/media ]; then
+  ui_print "! /product/media & /system/media not found"
+  ui_print " "
+fi
+
+# unmount
+unmount_mirror
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ post-fs-data.sh
@@ -0,0 +1,196 @@
+mount -o rw,remount /data
+MODPATH=${0%/*}
+
+# log
+exec 2>$MODPATH/debug-pfsd.log
+set -x
+
+# var
+API=`getprop ro.build.version.sdk`
+ABI=`getprop ro.product.cpu.abi`
+LIST32BIT=`getprop ro.product.cpu.abilist32`
+
+# function
+permissive() {
+if [ "$SELINUX" == Enforcing ]; then
+  if ! setenforce 0; then
+    echo 0 > /sys/fs/selinux/enforce
+  fi
+fi
+}
+magisk_permissive() {
+if [ "$SELINUX" == Enforcing ]; then
+  if [ -x "`command -v magiskpolicy`" ]; then
+	magiskpolicy --live "permissive *"
+  else
+	$MODPATH/$ABI/libmagiskpolicy.so --live "permissive *"
+  fi
+fi
+}
+sepolicy_sh() {
+if [ -f $FILE ]; then
+  if [ -x "`command -v magiskpolicy`" ]; then
+    magiskpolicy --live --apply $FILE 2>/dev/null
+  else
+    $MODPATH/$ABI/libmagiskpolicy.so --live --apply $FILE 2>/dev/null
+  fi
+fi
+}
+
+# selinux
+SELINUX=`getenforce`
+chmod 0755 $MODPATH/*/libmagiskpolicy.so
+#1permissive
+#2magisk_permissive
+#kFILE=$MODPATH/sepolicy.rule
+#ksepolicy_sh
+FILE=$MODPATH/sepolicy.pfsd
+sepolicy_sh
+
+# list
+PKGS="`cat $MODPATH/package.txt`
+       com.miui.rom:ui"
+for PKG in $PKGS; do
+  magisk --denylist rm $PKG 2>/dev/null
+  magisk --sulist add $PKG 2>/dev/null
+done
+if magisk magiskhide sulist; then
+  for PKG in $PKGS; do
+    magisk magiskhide add $PKG
+  done
+else
+  for PKG in $PKGS; do
+    magisk magiskhide rm $PKG
+  done
+fi
+
+# property
+PROP=`getprop ro.build.characteristics`
+if [ ! "$PROP" ]; then
+  resetprop -n ro.build.characteristics default
+fi
+
+# property
+DEVICE=`getprop ro.product.device`
+MODEL=`getprop ro.product.model`
+if [ "$DEVICE" == cancro ]; then
+  if ! echo "$MODEL" | grep "MI 3"\
+  && ! echo "$MODEL" | grep "MI 4"; then
+    resetprop -n ro.product.model "MI 4"
+  fi
+fi
+
+# permission
+chmod 0751 $MODPATH/system/bin
+FILES=`find $MODPATH/system/bin -type f`
+for FILE in $FILES; do
+  chmod 0755 $FILE
+done
+if [ "$API" -ge 26 ]; then
+  DIRS=`find $MODPATH/system/bin\
+             $MODPATH/vendor\
+             $MODPATH/system/vendor -type d`
+  for DIR in $DIRS; do
+    chown 0.2000 $DIR
+  done
+  FILES=`find $MODPATH/system/bin -type f`
+  for FILE in $FILES; do
+    chown 0.2000 $FILE
+  done
+  chcon -R u:object_r:system_lib_file:s0 $MODPATH/system/lib*
+  if [ -L $MODPATH/system/vendor ]\
+  && [ -d $MODPATH/vendor ]; then
+    chcon -R u:object_r:vendor_file:s0 $MODPATH/vendor
+    chcon -R u:object_r:vendor_configs_file:s0 $MODPATH/vendor/etc
+  else
+    chcon -R u:object_r:vendor_file:s0 $MODPATH/system/vendor
+    chcon -R u:object_r:vendor_configs_file:s0 $MODPATH/system/vendor/etc
+  fi
+fi
+
+# path
+ETC=/system/etc
+VETC=/vendor/etc
+MODETC=$MODPATH$ETC
+if [ -L $MODPATH/system/vendor ]\
+&& [ -d $MODPATH/vendor ]; then
+  MODVETC=$MODPATH$VETC
+else
+  MODVETC=$MODPATH/system$VETC
+fi
+
+# function
+patch_public_libraries() {
+for NAME in $NAMES; do
+  if ! grep $NAME $FILE; then
+    if echo "$ABI" | grep 64 && [ ! "$LIST32BIT" ]; then
+      echo "$NAME 64" >> $FILE
+    else
+      echo $NAME >> $FILE
+    fi
+  fi
+done
+chmod 0644 $FILE
+}
+patch_public_libraries_nopreload() {
+for NAME in $NAMES; do
+  if ! grep $NAME $FILE; then
+    if echo "$ABI" | grep 64 && [ ! "$LIST32BIT" ]; then
+      echo "$NAME 64 nopreload" >> $FILE
+    else
+      echo "$NAME nopreload" >> $FILE
+    fi
+  fi
+done
+chmod 0644 $FILE
+}
+
+# patch public libraries
+DES=public.libraries.txt
+rm -f `find $MODPATH -type f -name $DES`
+NAMES="libnativehelper.so libnativeloader.so libcutils.so
+       libutils.so libc++.so libandroidfw.so libui.so
+       libandroid_runtime.so libbinder.so"
+FILE=$MODETC/$DES
+#pcp -af $ETC/$DES $MODETC
+#ppatch_public_libraries
+NAMES="libmiui_runtime.so libmiuiblursdk.so libmiuinative.so
+       libmiuiblur.so libthemeutils_jni.so libshell_jni.so libshell.so
+       libmiuixlog.so libimage_arcsoft_4plus.so libstlport_shared.so"
+#ppatch_public_libraries_nopreload
+NAMES="libcdsprpc.so libadsprpc.so libOpenCL.so
+       libarcsoft_beautyshot.so libmpbase.so"
+FILE=$MODVETC/$DES
+#pcp -af $VETC/$DES $MODVETC
+#ppatch_public_libraries
+if [ "$API" -ge 26 ]; then
+  chcon u:object_r:vendor_configs_file:s0 $FILE
+  for NAME in $NAMES; do
+    if [ -L $MODPATH/system/vendor ]\
+    && [ -d $MODPATH/vendor ]; then
+      chcon u:object_r:same_process_hal_file:s0 $MODPATH/vendor/lib*/$NAME
+    else
+      chcon u:object_r:same_process_hal_file:s0 $MODPATH/system/vendor/lib*/$NAME
+    fi
+  done
+fi
+
+# cleaning
+FILE=$MODPATH/cleaner.sh
+if [ -f $FILE ]; then
+  . $FILE
+  mv -f $FILE $FILE.txt
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
